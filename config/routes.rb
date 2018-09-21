8fbf10be86333c18474ab836d0739b8f0a97113c
aaa <dododosa14@gmail.com>
2018-09-21 12:26:14 +0900

    ログインページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
