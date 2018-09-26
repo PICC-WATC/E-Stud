673cba54143087e5887acaf061e828f0a3b61676
ryuon <dododosa14@gmail.com>
2018-09-26 21:26:28 +0900

    ログイン時にチェックをすればブラウザにログイン状態を永続的に保持できるようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
-  get 'sessions/new'
+#  get 'sessions/new'
   get '/signup', to: 'users#new'
   get '/login',  to: 'sessions#new'
   post '/login', to: 'sessions#create'
