b4b56eda256976a8d1b5990f2ceb99d7f73c8cc9
ryuon <dododosa14@gmail.com>
2018-09-27 22:26:09 +0900

    テストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -45,4 +45,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
