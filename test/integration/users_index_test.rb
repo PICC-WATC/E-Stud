6df182becd4f8751714f730fcc4873e1dd4bafaa
ryuon <dododosa14@gmail.com>
2018-10-12 21:52:44 +0900

    とりあえずdevelopment環境ではアカウント有効化できるようになったはず

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @non_activated_user = users(:non_activated)
   end
 
   test "index including pagination" do
@@ -13,5 +14,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test "should not allow the not activated attribute" do
+    log_in_as(@non_activated_user)
+    assert_not @non_activated_user.activated?
+    get users_path
+    assert_select "a[href=?]", user_path(@non_activated_user), count: 0
+    get user_path(@non_activated_user)
+    assert_redirected_to root_url
   end
 end
