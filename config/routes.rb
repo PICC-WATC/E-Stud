285a6526e3986aecb7035251d785b3f9036d6c25
hzcyto <hzcyto@gmail.com>
2018-10-13 17:19:38 +0900

    progress:25%

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'course/course1' => "course#course2"
+  get 'course/course1' => "course#course1"
   root 'static_pages#home'
+  get "/home/course" => "home#course"
+  get "/home/test" => "home#test"
+  get "/home/data" => "home#data"
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
