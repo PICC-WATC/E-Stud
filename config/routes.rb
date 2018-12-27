1c13385ce4763debf29ba4b9b24f1eba39f4401f
ryuon <dododosa14@gmail.com>
2018-12-27 18:55:19 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login',  to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessins#destroy'
-  #delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   resources :account_activations, only: [:edit]
