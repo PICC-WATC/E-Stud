a10911fb717ec1cf5cdd646355cc8f6e3ce65a1c
ryuon <dododosa14@gmail.com>
2018-12-27 17:34:12 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
+  root '/', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
