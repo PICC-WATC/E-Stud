61a3cd5096b8ed660fb81e0d4c152429214b59f2
ryuon <dododosa14@gmail.com>
2018-12-27 19:24:40 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   host = 'https://mighty-earth-93615.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp-sendgrid.net',
+    :address => 'smtp.sendgrid.net',
     :port    => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
